aa3b8c8ccc51f8429fa4aac5a6634d1d8de26726
Zane Fink <finkzane@gmail.com>
2023-04-18 14:51:43 -0500

    initialize setup script that downloads data

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -d "benchmarks/input_data" ]; then
+  echo "Directory benchmarks/input_data already exists. Skipping download and unzip."
+else
+  # Download the archive using wget
+  wget https://uofi.box.com/shared/static/7p8eb0ppw6zhtdr7krhiw9kat52lmwez.gz
+
+  # Unzip the archive using tar
+  tar -xzf 7p8eb0ppw6zhtdr7krhiw9kat52lmwez.gz
+
+  # Create the 'benchmarks' directory if it doesn't exist
+  mkdir -p benchmarks
+
+  # Move the 'input_data' directory to the 'benchmarks' directory
+  mv input_data benchmarks/
+fi
+
